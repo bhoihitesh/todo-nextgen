b9794716e03039e2f0bc23e035275d533d4903c0
softdev1844 <soft.dev1844@gmail.com>
2024-12-22 17:48:40 +0530

    UI changes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,24 +1,33 @@
 "use client";
-import { Col, Row } from 'antd';
-import { useRouter } from 'next/navigation';
-import React from 'react'
-import { useSelector } from 'react-redux';
+import { Col, Row } from "antd";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
-  const router = useRouter()
+  const router = useRouter();
 
-  const {isSidebarOpen} = useSelector((state: any)=>state.myslice);
+  const { isSidebarOpen } = useSelector((state: any) => state.myslice);
   return (
-    <div className={`${isSidebarOpen ? ' lg:d-block lg:w-[250px] md:w-[350px] w-[700px] sidebar-container text-white bg-gray-900 transition-all ease-in' : 'w-[0px] d-none transition-all ease-linear'}`}>
-      { isSidebarOpen && 
-      <Row>
-        <Col>
-        <h1 className='cursor-pointer' onClick={()=>router.push('/dashboard')}>Dashboard</h1>  
-        </Col>
-      </Row>
-      }
+    <div
+      className={`${
+        isSidebarOpen
+          ? " lg:d-block lg:w-[250px] md:w-[350px] w-[700px] sidebar-container text-white bg-gray-900 transition-all ease-in"
+          : "w-[0px] d-none transition-all ease-linear"
+      }`}
+    >
+      {isSidebarOpen && (
+        <ul className="text-center w-full px-1">
+          <li
+            className="cursor-pointer py-2 bg-gray-600 rounded-md"
+            onClick={() => router.push("/dashboard")}
+          >
+            Dashboard
+          </li>
+        </ul>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
