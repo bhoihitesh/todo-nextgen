8c9bbff478e1fbc0ea5229733bfc356d6cde408f
softdev1844 <soft.dev1844@gmail.com>
2025-03-21 07:04:37 +0530

    comment added to the APIs

--- src/customeAPIs/page.ts
+++ src/customeAPIs/page.ts
@@ -5,10 +5,12 @@ export const getUsers = async() => {
     return await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/users`)
 }
 
+// login API
 export const login = async(user: userType) => {
     return await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/login`,user, {withCredentials: true});
 }
 
+// logout API
 export const logout = async()=>{
     return await axios.delete(`${process.env.NEXT_PUBLIC_BASE_URL}/logout`,{withCredentials: true})
 }
