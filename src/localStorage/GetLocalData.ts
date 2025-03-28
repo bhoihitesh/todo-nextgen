7a7a22257b65f723ef748fbdcf00be52d2f6c508
softdev1844 <soft.dev1844@gmail.com>
2025-03-29 03:18:27 +0530

    updated the localstorage file to ensure the window object is present while accessing it

--- src/localStorage/GetLocalData.ts
+++ src/localStorage/GetLocalData.ts
@@ -2,15 +2,24 @@
 
 // Get the auth object
 export const getAuth = () => {
+    if (typeof window === "undefined") {
+        return null; // Prevents error during SSR
+      }
     return localStorage.getItem('auth');
 }
 
 // Set the auth object
 export const setAuth=(payload: string)=>{
+    if (typeof window === "undefined") {
+        return null; // Prevents error during SSR
+      }
     return localStorage.setItem('auth', JSON.stringify(payload))
 }
 
 // Clear all data from localStorage
 export const clearLocalStorage = () => {
+    if (typeof window === "undefined") {
+        return null; // Prevents error during SSR
+      }
     return localStorage.clear();
 }
